f8789d78c7eddb6c130a8059fd95fb9476d987db
claire.wahl <claire.wahl@virtual-expo.com>
2017-12-11 15:52:48 +0100

    flop

--- load.py
+++ load.py
@@ -8,32 +8,15 @@ import os, sys
 from itertools import islice
 
 
-# concatenate all its arguments, adding '/' between elements
-def makepath(*arg):
-    result = ""
-    for i in arg:
-        if result == "":
-            result += i
-        else:
-            result += ( '/' + str(i) )
-            return result
-
 def cut_line_1(fin,tmp_file):
     with open(fin, 'r') as f0:
         data = f0.read().splitlines(True)
     with open(tmp_file, 'w') as fout:
         fout.writelines(data[1:])
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Loads yaml')
-    parser.add_argument('-y', '--yamldir', required=True, default='./yaml', help='path to the yaml directory', type=str)
-    parser.add_argument('-t', '--tmpdir', required=True, default='./tmpdir', help='path to the temporary directory', type=str)
-
-    args = parser.parse_args()
-
-    tmp_file = args.tmpdir + '/file.yaml'
-    path = args.yamldir + '/node/' + 'velo1dblx01.virtual-expo.com.yaml'
+def puppet_to_rundeck(yaml_node_dir,outputdir,node):
+    tmp_file = outputdir + '/tmp/tmpfile.yaml'
+    path = yaml_node_dir + node
     print("file: %s" % path)
 
     if not os.path.isfile(path):
@@ -41,11 +24,27 @@ def main():
         sys.exit(1)
     else:
         cut_line_1(path,tmp_file)
-        with open(tmp_file, 'r') as f1:
-            try:
-                print(yaml.load(f1))
-            except:
-                raise
+        f = open(tmp_file, 'r')
+        yaml_data = yaml.load(f)
+
+        tags = yaml_data['parameters']['datacenter'] + ',' + yaml_data['parameters']['node_environment'] + ',' + yaml_data['parameters']['node_type']
+        d = {yaml_data['parameters']['hostname']:{'hostname':yaml_data['name'], 'osFamily':yaml_data['parameters']['osfamily'], 'osVersion':yaml_data['parameters']['os']['release']['full'], 'osName':yaml_data['parameters']['os']['lsb']['distdescription'], 'tags':tags}}
+        f.close()
+
+        with open(outputdir + '/node/' + node + '.yaml', 'w') as result_file:
+            yaml.dump(d, result_file, default_flow_style=False)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Loads yaml')
+    parser.add_argument('-y', '--yamldir', required=True, default='./yaml', help='path to the yaml directory', type=str)
+    parser.add_argument('-o', '--outputdir', required=True, default='./outdir', help='path to the outgoing directory', type=str)
+
+    args = parser.parse_args()
+    yaml_node_dir = args.yamldir + '/node/'
+
+    for node in os.listdir(yaml_node_dir):
+        puppet_to_rundeck(yaml_node_dir,args.outputdir,node)
 
 if __name__ == "__main__":
     main()
