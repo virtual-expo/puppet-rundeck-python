1005898325b7c4a14aec46438efaed6302b458d2
Thomas Lecomte <thomas.lecomte@virtual-expo.com>
2018-02-12 09:57:18 +0100

    implemented shit to keep the destfile available

--- code/node_loop.py
+++ code/node_loop.py
@@ -5,10 +5,12 @@
 #, sys, time
 
 import os
+from stat import *
 from datetime import datetime, timedelta
 import glob
 from code.helper import *
 from code.generate_yaml import *
+import tempfile
 
 def generate_node(filehandle,outputfile,max_age,path_to_node):
     if (datetime.now() - datetime.fromtimestamp(os.path.getmtime(path_to_node))) > timedelta(days = max_age):
@@ -24,10 +26,17 @@ def node_loop(yaml_node_dir, outfile, max_age):
 
     listing = glob.glob(yaml_node_dir + '/*.yaml')
 
-    filehandle = open(outfile, 'w')
+    filehandle = tempfile.NamedTemporaryFile(mode='w', prefix='puppet-to-rundeck', dir=os.path.dirname(outfile))
+    os.chmod(filehandle.name, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
     for path_to_node in listing:
         generate_node(filehandle, outfile, max_age, path_to_node)
 
     logv('end of the loop')
+
+    # remove the real outfile and create a new hardlink
+    os.remove(outfile)
+    os.link(filehandle.name, outfile)
+
+    # python automagically removes the tempfile
     filehandle.close
