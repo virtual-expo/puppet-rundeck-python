62d3bb2d063fd938b21336cf61f0324245d617d4
claire.wahl <claire.wahl@virtual-expo.com>
2018-04-26 11:12:13 +0200

    no default value for outfile

--- puppet_to_rundeck.py
+++ puppet_to_rundeck.py
@@ -10,7 +10,7 @@ from code.node_loop import node_loop
 def main():
     parser = argparse.ArgumentParser(description='Reads in puppet yaml dir to make a list of nodes for rundeck')
     parser.add_argument('-v', '--verbose', default=False, action='store_true', help='verbose logging')
-    parser.add_argument('-o', '--outfile', default='/srv/http/puppet.virtual-expo.com/all.yaml', type=str, help='output file')
+    parser.add_argument('-o', '--outfile', type=str, help='output file')
     parser.add_argument('-i', '--inputdir', default='/var/lib/puppet/yaml/node', type=str, help='input directory')
     parser.add_argument('-m', '--maxage', type=int, default=7, help='max age of input node files (days)')
 
